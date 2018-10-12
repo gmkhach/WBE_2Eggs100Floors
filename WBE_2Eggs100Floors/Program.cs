35e5e11878988b524f7f3ab599e2c8f16394c4f6
Gabriel Khachatryan <gmkhach@gmail.com>
2018-10-11 23:40:01 -0400

    brute-force method is implemented

--- WBE_2Eggs100Floors/Program.cs
+++ WBE_2Eggs100Floors/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * A building has 100 floors. One of the floors is the highest floor an egg can be dropped from without breaking.
+ * If an egg is dropped from above that floor, it will break. If it is dropped from that floor or below, 
+ * it will be completely undamaged and you can drop the egg again.
+ * Given two eggs, find the highest floor an egg can be dropped from without breaking, with as few drops as possible.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +17,67 @@ namespace WBE_2Eggs100Floors
     {
         static void Main(string[] args)
         {
+            do
+            {
+                try
+                {
+                    Console.Write("\nEnter an integer between 1 and 100\n\n>>> ");
+                    int input = int.Parse(Console.ReadLine().Trim());
+                    FindHighestSafeFloor(input);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("\n" + ex.Message);
+                }
+                Console.Write("\nPress Enter to try another floor...");
+                Console.ReadLine();
+                Console.Clear();
+            } while (true);
+        }
+
+        static void FindHighestSafeFloor(int highest)
+        {
+            int testFloor;
+            bool keepLooping = true;
+            // dropping the first egg from the 50th floor.
+            // if it doesn't break then start dropping the next egg from every other floor above 50th until it breaks.
+            if (highest >= 50)
+            {
+                Console.WriteLine("\nTest 1: The 1st Egg was dropped from floor #50 and it didn't break");
+                testFloor = 51;
+                do
+                {
+                    if (highest >= testFloor)
+                    {
+                        Console.WriteLine($"\nTest {testFloor - 49}: The 2nd egg was dropped from floor #{testFloor} and it didn't break");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\nTest {testFloor - 49}: The 2nd egg was dropped from floor #{testFloor} and it broke");
+                        keepLooping = false;
+                    }
+                    testFloor++;
+                } while (keepLooping);
+            }
+            // if it breaks then start dropping the other egg from the 1st floor and work your way up until it breaks.
+            else
+            {
+                Console.WriteLine("\nTest 1: The 1st Egg was dropped from floor #50 and it broke");
+                testFloor = 1;
+                do
+                {
+                    if (highest >= testFloor)
+                    {
+                        Console.WriteLine($"\nTest {testFloor + 1}: The 2nd egg was dropped from floor #{testFloor} and it didn't break");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\nTest {testFloor + 1}: The 2nd egg was dropped from floor #{testFloor} and it broke");
+                        keepLooping = false;
+                    }
+                    testFloor++;
+                } while (keepLooping);
+            }
         }
     }
 }
